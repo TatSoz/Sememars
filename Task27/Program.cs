2c6203272d292933c5c94a94786b62c9bc8f76d6
tatyana <stv24@list.ru>
2022-09-01 15:12:23 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12    */
+
+int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 0; number >= 1; i++ )
+    {
+        sum+= number % 10;
+        number = number/10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{num} -> {Sum(num)}");
