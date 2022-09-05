cac5999a8bb1b014af0a57aa604bb1b1709a066b
tatyana <stv24@list.ru>
2022-09-05 19:18:07 +0300

    Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿/*  Задайте массив заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.       */
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+void EvenNumber(int[] array)
+{
+    int evenNum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        evenNum++;
+    }
+    Console.WriteLine($"{evenNum}");
+}
+
+int[] myArray = GetArray(20, 100, 999);
+Console.WriteLine(String.Join(", ", myArray));
+EvenNumber(myArray);
