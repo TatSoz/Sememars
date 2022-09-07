aab73601f3dd53128a3fd73d8f4f2dcb237a5d3b
tatyana <stv24@list.ru>
2022-09-07 12:30:23 +0300

    Task38(1)

---
+++ Task38(1)/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива*/
+
+double[] GetArray(int size)
+{
+    double[] array = new double[size];
+  
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble()*100;
+    }
+    return array;
+}
+
+double NumMin(double[] array)
+{
+    double numMin = array[0];
+    for (int i =0; i < array.Length; i++)
+    {
+        if (numMin > array[i])
+        numMin = array[i];
+    }
+    return numMin;
+}    
+double NumMax(double[] array)
+{
+    double numMax = array[0];
+    for (int i =0; i < array.Length; i++)
+    {
+        if (numMax < array[i])
+        numMax = array[i];
+    }
+    return numMax;
+   
+}
+
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine());
+double[] myArray = GetArray(N);
+Console.WriteLine(String.Join(", ", myArray));
+Console.WriteLine($"Максимальное значение: = {NumMax(myArray)}");
+Console.WriteLine($"Минимальное значение: = {NumMin(myArray)}");
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: = {NumMax(myArray) - NumMin(myArray)}");
