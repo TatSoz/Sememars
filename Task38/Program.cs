d22bba2c667f2a312a41f85257678ea4361c4541
tatyana <stv24@list.ru>
2022-09-07 12:25:29 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива*/
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1)/10;
+    }
+    return array;
+}
+
+double NumMin(double[] array)
+{
+    double numMin = array[0];
+    for (int i =0; i < array.Length; i++)
+    {
+        if (numMin > array[i])
+        numMin = array[i];
+    }
+    return numMin;
+}
+
+double NumMax(double[] array)
+{
+    double numMax = array[0];
+    for (int i =0; i < array.Length; i++)
+    {
+        if (numMax < array[i])
+        numMax = array[i];
+    }
+    return numMax;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("Введите начало диапазона: ");
+int startRange = int.Parse(Console.ReadLine());
+Console.Write("Введите конец диапазона: ");
+int endRange = int.Parse(Console.ReadLine());
+
+double[] myArray = GetArray(N, startRange, endRange);
+Console.WriteLine(String.Join(", ", myArray));
+Console.WriteLine($"Максимальное значение: = {NumMax(myArray)}");
+Console.WriteLine($"Минимальное значение: = {NumMin(myArray)}");
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: = {NumMax(myArray) - NumMin(myArray)}");
