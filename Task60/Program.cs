9b84f3330a0f71519b35d3caca10d655e4016fe7
tatyana <stv24@list.ru>
2022-09-17 01:19:37 +0300

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив,
+добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2       66(0,0,0) 25(0,1,0)
+                                34(1,0,0) 41(1,1,0)
+                                27(0,0,1) 90(0,1,1)
+                                26(1,0,1) 55(1,1,1)       */
+
+void CreateArray(int[,,] array3D)
+{
+  int[] array = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+  int  element;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    array[i] = new Random().Next(10, 100);
+    element = array[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (array[i] == array[j])
+        {
+          array[i] = new Random().Next(10, 100);
+          j = 0;
+          element = array[i];
+        }
+          element = array[i];
+      }
+    }
+  }
+  
+  
+  int count = 0; 
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        array3D[i, j, k] = array[count];
+        count++;
+      }
+    }
+  }
+}
+
+void WriteArray (int[,,] array3D)
+{
+  for (int i = 0; i < array3D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+      for (int k = 0; k < array3D.GetLength(2); k++)
+      {
+        Console.Write($"{array3D[i, j, k]} ({i},{j},{k}) ");
+      }
+      Console.WriteLine();
+    }
+    //Console.WriteLine();
+  }
+}
+
+Console.WriteLine("Введите количество строк первой матрицы x: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов первой матрицы y: ");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк второй матрицы z: ");
+int z = int.Parse(Console.ReadLine());
+
+int[,,] array3D = new int[x, y, z];
+CreateArray(array3D);
+WriteArray (array3D);
